ac7cd3221764f67c2e9890b42a615e16dd76e93d
QQ <cchangwo@yahoo.com.tw>
2020-11-25 00:50:26 +0800

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
 
   const [movies, setMovie] =useState(['Movie 1 ','Movie 2']);
 
+  useEffect(()=>{
+    fetch("http://127.0.0.1:8000/api/movies/",{
+      method:'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Token 00899e358115a9ecd55a2fec3a88b74c28ed6076'
+      }
+    }).then( resp => resp.json() )
+    .then(resp => setMovie(resp))
+    .catch(error => console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,7 +25,7 @@ function App() {
       <div className="layout">
           <div>
             { movies.map(movie=>{
-              return <h2> {movie} </h2>
+              return <h2> {movie.title} </h2>
             })}
           </div>
           <div>Movie details</div>

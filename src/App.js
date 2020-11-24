d931525c0bbf7716b39cb5b2385b3fc634f7b32c
QQ <cchangwo@yahoo.com.tw>
2020-11-25 00:17:20 +0800

    Layout & List component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] =useState(['Movie 1 ','Movie 2']);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Movie rater</h1>
       </header>
+      <div className="layout">
+          <div>
+            { movies.map(movie=>{
+              return <h2> {movie} </h2>
+            })}
+          </div>
+          <div>Movie details</div>
+      </div>
     </div>
   );
 }

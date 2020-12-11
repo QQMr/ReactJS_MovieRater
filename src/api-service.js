d8ffef957e0fe0b0a2fdb32a4b802402729e43a0
QQ <cchangwo@yahoo.com.tw>
2020-12-12 00:51:39 +0800

    Login page

--- src/api-service.js
+++ src/api-service.js
@@ -1,6 +1,16 @@
 const TOKEN = "00899e358115a9ecd55a2fec3a88b74c28ed6076"
 
 export class API{
+
+    static LoginUser(body) {
+      return fetch(`http://127.0.0.1:8000/auth/`,{
+        method:'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body)
+      }).then( resp => resp.json() )
+    }
     static updateMovie(mov_id,body) {
         return fetch(`http://127.0.0.1:8000/api/movies/${mov_id}/`,{
             method:'PUT',
--- src/components/auth.js
+++ src/components/auth.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {API} from "../api-service"
 
 function Auth(){
+
+    const [ username,setUsername] = useState('');
+    const [ password,setPssword] = useState('');
+
+    const loginClicked = () => {
+        console.log( {username,password} );
+        API.LoginUser( {username,password} )
+            .then(resp => console.log(resp))
+            .catch( error => console.log(error))
+    }
     return(
-        <h1>AUTH</h1>
+        <div>
+                <label htmlFor="username" >username</label><br/>
+                <input id="username" type="text" placeholder="username" value={username}
+                        onChange = { evt => setUsername(evt.target.value) }
+                /><br/>
+                <label htmlFor="password" type="text">password</label><br/>
+                <input id="password" type="password" placeholder="password"
+                            value ={password}
+                            onChange = { evt => setPssword(evt.target.value) }
+                ></input>
+                <br/>
+               
+                <button onClick = { loginClicked } >Login</button>
+                    
+               
+        </div>
     )
 }
 

b715cff72adb011e77a1486f01d5b219b40926dc
QQ <changwo@yahoo.com.tw>
2020-12-13 01:58:15 +0800

    Register users

--- src/components/auth.js
+++ src/components/auth.js
@@ -6,6 +6,7 @@ function Auth(){
 
     const [ username,setUsername] = useState('');
     const [ password,setPssword] = useState('');
+    const [ isLoginView,setIsLoginView] = useState(false);
 
     const [token, setToken] = useCookies(['mr-token']);
 
@@ -21,8 +22,16 @@ function Auth(){
             .then(resp => {console.log(resp); setToken('mr-token',resp.token); } )
             .catch( error => console.log(error))
     }
+
+    const registerUserClicked = () => {
+        API.registerUser( {username,password} )
+            .then(resp => {console.log(resp); loginClicked(); } )
+            .catch( error => console.log(error))
+    }
+
     return(
         <div>
+                {isLoginView? <h1>Login</h1> : <h1>Register</h1> }
                 <label htmlFor="username" >username</label><br/>
                 <input id="username" type="text" placeholder="username" value={username}
                         onChange = { evt => setUsername(evt.target.value) }
@@ -33,10 +42,18 @@ function Auth(){
                             onChange = { evt => setPssword(evt.target.value) }
                 ></input>
                 <br/>
+
+                {isLoginView? 
+                    <button onClick = { loginClicked } >Login</button> :
+                    <button onClick = { registerUserClicked } >Register</button>
+                }
                
-                <button onClick = { loginClicked } >Login</button>
+                
                     
-               
+                {isLoginView? 
+                    <p onClick={()=>{setIsLoginView(false)}}>You don't have an account? Register here</p> :
+                    <p onClick={()=>{setIsLoginView(true)}}>You already have an account? Login here</p>
+                }               
         </div>
     )
 }
